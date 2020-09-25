1eeeabe9247fd974382176d4474644ab1fe7e187
mrkai1253 <asishshajithomas@gmail.com>
2020-09-25 12:38:07 +0530

    added userrepo and user handlers

--- main.go
+++ main.go
@@ -4,20 +4,32 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/asishshaji/go-voting-api/go/src/github.com/asishshaji/go-voting-api/internal/config"
+	"github.com/asishshaji/go-voting-api/go/src/github.com/asishshaji/go-voting-api/internal/database"
+	"github.com/asishshaji/go-voting-api/go/src/github.com/asishshaji/go-voting-api/user"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
-
 	router := chi.NewRouter()
+	router.Use(middleware.Logger)
 	router.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Healthy")
+		fmt.Fprint(w, "healthy")
 	})
-	fmt.Println("Starting server")
 
-	err := http.ListenAndServe(":9090", router)
+	db, err := database.NewDB(config.NewConfig())
+
 	if err != nil {
-		fmt.Printf("Error starting server : %s", err)
+		panic(err)
 	}
 
+	user.RegisterRoutes(db, router)
+
+	fmt.Println("Starting server...")
+
+	err = http.ListenAndServe(":8080", router)
+	if err != nil {
+		fmt.Printf("Error staring server: %s", err)
+	}
 }
